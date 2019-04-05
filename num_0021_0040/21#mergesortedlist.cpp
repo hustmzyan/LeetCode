6ab22784cdbde7873c93a8ffa670ee8cf90fc745
mzyan <mengzhao.yan@foxmail.com>
2019-04-05 23:38:31 +0800

    21#

---
+++ num_0021_0040/21#mergesortedlist.cpp
@@ -0,0 +1,82 @@
+/**
+ * 21. Merge Two Sorted Lists
+ */
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* res;
+        ListNode* cur;
+        bool flag = true;
+        while(l1->next && l2->next){
+            if(l1->val <= l2->val){
+                if(flag){
+                    res = l1;
+                    cur = res;
+                    flag = false;
+                }else{
+                    cur->next = l1;
+                    cur = l1;
+                }
+                l1 = l1->next;
+            }else{
+                /* code */
+                if(flag){
+                    res = l2;
+                    cur = res;
+                    flag = false;
+                }else{
+                    cur->next = l2;
+                    cur = l2;
+                }
+                l2 = l2->next;
+            }
+            
+        }
+        while(l1->next){
+            cur->next = l1;
+            cur = l1;
+        }
+        while(l2->next){
+            cur->next = l2;
+            cur = l2;
+        }
+
+        return res;
+    }
+};
+
+int main(){
+    ListNode node1_1(1);
+    ListNode node1_2(2);
+    ListNode node1_3(4);
+    node1_1.next = &node1_2;
+    node1_2.next = &node1_3;
+
+    ListNode node2_1(1);
+    ListNode node2_2(3);
+    ListNode node2_3(4);
+    node2_1.next = &node2_2;
+    node2_2.next = &node2_3;
+
+
+    Solution s;
+    ListNode* result;
+    result = s.mergeTwoLists(&node1_1, &node2_1);
+
+    while(result->next){
+        cout << result->val << "->";
+    }
+    cout << endl;
+    return 0;
+}
