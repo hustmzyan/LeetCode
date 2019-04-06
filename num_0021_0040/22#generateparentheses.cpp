998126c68365d117d7831b2bd7c98808f5540950
mzyan <mengzhao.yan@foxmail.com>
2019-04-06 11:28:30 +0800

    22#

---
+++ num_0021_0040/22#generateparentheses.cpp
@@ -0,0 +1,22 @@
+/**
+ * 22. Generate Parentheses
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
