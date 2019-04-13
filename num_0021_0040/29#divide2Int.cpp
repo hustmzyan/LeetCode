cbb22b930dae0d62c9b660dfea391c4df64dad62
mzyan <mengzhao.yan@foxmail.com>
2019-04-13 16:08:22 +0800

    modify #29, add #30 not solve

--- num_0021_0040/29#divide2Int.cpp
+++ num_0021_0040/29#divide2Int.cpp
@@ -10,32 +10,22 @@ using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        bool flag = true;
-        int res = -1;
-        if(dividend == 0){
-            return 0;
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
         }
-        if((divisor < 0 && dividend > 0) || (divisor > 0 && dividend < 0)){
-            divisor = abs(divisor);
-            dividend = abs(dividend);
-            flag = false;
-        }
-        if(dividend < 0){
-            while(dividend < 0){
-                dividend -= divisor;
-                res++;
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs) {
+            // 通过位运算方式，层层精确求解
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd) {
+                temp <<= 1;
+                m <<= 1;
             }
-        }else{
-            while(dividend > 0){
-                dividend -= divisor;
-                res++;
-            }
+            dvd -= temp;
+            ans += m;
         }
-        if(flag){
-            return res;
-        }else{
-            return -res;
-        }
+        return sign * ans;
     }
 };
 
---
+++ num_0021_0040/30#findConcatenationSubstring.cpp
@@ -0,0 +1,26 @@
+/**
+ * 30. Substring with Concatenation of All Words
+ * 
+ * You are given a string, s, and a list of words, words, that are all of the same length. 
+ * Find all starting indices of substring(s) in s 
+ * that is a concatenation of each word in words exactly once 
+ * and without any intervening characters.
+ * 
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        
+    }
+};
+
+int main(){
+
+    return 0;
+}
