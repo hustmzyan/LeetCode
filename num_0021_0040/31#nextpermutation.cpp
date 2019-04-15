030046349d71755c0acfb1927684a3ae235b0bf6
mzyan <mengzhao.yan@foxmail.com>
2019-04-15 11:35:55 +0800

    add 31#

---
+++ num_0021_0040/31#nextpermutation.cpp
@@ -0,0 +1,31 @@
+/**
+ * 31. Next Permutation
+ * 
+ * Implement next permutation, 
+ * which rearranges numbers into the lexicographically next greater permutation of numbers
+ * 
+ * If such arrangement is not possible, 
+ * it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * 
+ * The replacement must be in-place and use only constant extra memory.
+ * 
+ * Here are some examples. 
+ * Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ */
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+
+    return 0;
+}
