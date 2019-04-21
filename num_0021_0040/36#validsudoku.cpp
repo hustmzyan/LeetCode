20c7d6ada4a05361dae5282020444192d10848de
mzyan <mengzhao.yan@foxmail.com>
2019-04-21 20:03:17 +0800

    36# add

---
+++ num_0021_0040/36#validsudoku.cpp
@@ -0,0 +1,28 @@
+/**
+ * 36. Valid Sudoku
+ * 
+ * Determine if a 9x9 Sudoku board is valid. 
+ * Only the filled cells need to be validated according to the following rules:
+ * 
+ * 1、Each row must contain the digits 1-9 without repetition.
+ * 2、Each column must contain the digits 1-9 without repetition.
+ * 3、Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * 
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+    }
+};
+
+int main(){
+
+    return 0;
+}
