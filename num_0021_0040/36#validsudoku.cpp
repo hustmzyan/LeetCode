744f291c282d073aa0159115a8c223b16e0943cb
mzyan <mengzhao.yan@foxmail.com>
2019-04-21 20:18:17 +0800

    35# update

--- num_0021_0040/36#validsudoku.cpp
+++ num_0021_0040/36#validsudoku.cpp
@@ -18,11 +18,42 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
         
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                        return false;
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                }
+        
+        return true;
     }
 };
 
 int main(){
+
+    vector<vector<char>> board = {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+        };
+
+    Solution s;
+
+    bool flag = s.isValidSudoku(board);
+
+    cout << flag << endl;
+
 
     return 0;
 }
