d1458f5cbcf719a4b739c7cf93285c82b1dbd0b0
mzyan <mengzhao.yan@foxmail.com>
2019-04-28 20:54:56 +0800

    添加注释

--- num_0021_0040/36#validsudoku.cpp
+++ num_0021_0040/36#validsudoku.cpp
@@ -18,13 +18,18 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        // 是否填充标记
         int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
         
+        // 遍历
         for(int i = 0; i < board.size(); ++ i)
             for(int j = 0; j < board[i].size(); ++ j)
+                // 判断不为空
                 if(board[i][j] != '.')
                 {
-                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    int num = board[i][j] - '0' - 1; // num为board[i][j]对应的下表，如：5—>4
+                    int k = i / 3 * 3 + j / 3;  // 9个9宫格的下标
+                    // 判断横向、纵向、9宫格内是否都已使用
                     if(used1[i][num] || used2[j][num] || used3[k][num])
                         return false;
                     used1[i][num] = used2[j][num] = used3[k][num] = 1;
--- num_0021_0040/37#sudokusolver.cpp
+++ num_0021_0040/37#sudokusolver.cpp
@@ -1,6 +1,8 @@
 /**
  * 37. Sudoku Solver
  * 
+ * 填充数独9宫格
+ * 
  */
 
 #include <stdio.h>
@@ -14,32 +16,55 @@ public:
         solveSudoku(board, 0, 0);
     }
 
-    bool check(vector<vector<char>> &board, int i, int j, char val){
-        int row = i - i%3, column = j - j%3;
-        for(int x=0; x<9; x++) if(board[x][j] == val) return false;
-        for(int y=0; y<9; y++) if(board[i][y] == val) return false;
-        for(int x=0; x<3; x++)
-        for(int y=0; y<3; y++)
-            if(board[row+x][column+y] == val) return false;
-        return true;
-    }
-
     bool solveSudoku(vector<vector<char>> &board, int i, int j){
+        // 遍历完
         if(i==9) return true;
+        // 当前行遍历完
         if(j==9) return solveSudoku(board, i+1, 0);
+        // 当前位置已填充
         if(board[i][j] != '.') return solveSudoku(board, i, j+1);
-
+        // 轮流测试填入数据
         for(char c='1'; c<='9'; c++)
         {
             if(check(board, i, j, c))
             {
                 board[i][j] = c;
+                // 递归进入下层
                 if(solveSudoku(board, i, j+1)) return true;
+                // 下层失败，重置当前格数据
                 board[i][j] = '.';
             }
-        }
-            
+        }     
         return false;
+    }
+
+    // 在 board[i][j] 填充 val 是否可以？
+    bool check(vector<vector<char>> &board, int i, int j, char val)
+    {
+        // 计算出该坐标对应小9宫格第一个格的下标
+        int row = i - i % 3, column = j - j % 3;
+        // 判断当前行是否有该数据已存在
+        for (int x = 0; x < 9; x++)
+        {
+            if (board[x][j] == val)
+                return false;
+        }
+        // 判断当前列是否有该数据已存在
+        for (int y = 0; y < 9; y++)
+        {
+            if (board[i][y] == val)
+                return false;
+        }
+        // 判断当前9宫格是否有该数据已存在
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[row + x][column + y] == val)
+                    return false;
+            }
+        }
+        return true;
     }
 };
 
