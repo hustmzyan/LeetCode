5c5d5abaa6c267fb36a0eb2407b2c134871ce115
mzyan <mengzhao.yan@foxmail.com>
2019-05-01 16:53:21 +0800

    39#

---
+++ num_0021_0040/39#conbinationsum.cpp
@@ -0,0 +1,65 @@
+/**
+ * 39. Combination Sum
+ * 
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > re;
+
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        vector<int> comb;
+        for (int i = 0; i < candidates.size(); i++)
+            fun(candidates, target - candidates[i], i, comb);
+        return re;
+    }
+
+    void fun(vector<int> candidates, int target, int index, vector<int> comb)
+    {
+        comb.push_back(candidates[index]);
+        if (target == 0)
+        {
+            re.push_back(comb);
+            comb.pop_back();
+            return;
+        }
+        else if (target < 0)
+        {
+            comb.pop_back();
+            return;
+        }
+        else
+        {
+            for (int i = index; i < candidates.size(); i++)
+            {
+                fun(candidates, target - candidates[i], i, comb);
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> candidates;
+    candidates.push_back(2);
+    candidates.push_back(3);
+    candidates.push_back(5);
+
+    Solution s;
+
+    vector<vector<int> > res = s.combinationSum(candidates, 8);
+
+    for (int i; i < res.size(); i++) {
+        cout << '[';
+        for (int j = 0; j < res[i].size(); j++) {
+            cout << res[i][j] << ',';
+        }
+        cout << ']' << endl;
+    }
+
+    return 0;
+}
