16fb434260151408146c74dbe9827f3cde795c47
mzyan <mengzhao.yan@foxmail.com>
2019-05-12 20:22:42 +0800

    update 45#

--- num_0041_0060/45#jumpgame2.py
+++ num_0041_0060/45#jumpgame2.py
@@ -26,9 +26,26 @@ def jump(nums):
         lastIdx, nextIdx = nextIdx, max(i + nums[i] for i in range(lastIdx, nextIdx + 1))
     return ans
 
+def jump2(nums):
+    n = len(nums)
+    if n < 2:
+        return 0
+    step = 0
+    lc = 0  # current step can reach lc
+    ln = nums[0]  # next jump will reach ln at most
+    for i in range(n):
+        if i > lc:  # if index beyond current step, then update lc and step to move forward
+            lc = ln
+            step += 1
+        if i + nums[i] > ln: # if i's next jump region beyond ln, update ln then.
+            ln = i + nums[i]
+        # print(i, lc, ln, step)
+        if ln >= n-1: # when ln can reach the end, just one more step will finish the game
+            return step + 1
+
 def main():
     nums = [2, 3, 1, 1, 4]
-    print(jump(nums))
+    print(jump2(nums))
 
 if __name__ == "__main__":
     main()
