bfd0134353d3955966b26305c0f1cd743d55918e
mzyan <mengzhao.yan@foxmail.com>
2019-05-13 20:56:26 +0800

    add 47#

--- num_0041_0060/47#permutation2.py
+++ num_0041_0060/47#permutation2.py
@@ -1,7 +1,5 @@
 """
-46. Permutations
-
-Given a collection of distinct integers, return all possible permutations.
+47. Permutations 2
 """
 
 def permute(nums):
@@ -9,10 +7,16 @@ def permute(nums):
             for i, n in enumerate(nums)
             for p in permute(nums[:i] + nums[i + 1:])] or [[]]
 
+def permuteUnique(nums):
+    res = []
+    for l in permute(nums):
+        if l not in res:
+            res.append(l)
+    return res
 
 def main():
     nums = [1, 1, 2]
-    print(permute(nums))
+    print(permuteUnique(nums))
 
 if __name__ == "__main__":
     main()
