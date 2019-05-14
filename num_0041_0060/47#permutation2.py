d2ec2d480bb31de119cd1fa63ed5029b902bc019
mzyan <mengzhao.yan@foxmail.com>
2019-05-14 21:00:54 +0800

    update 47

--- num_0041_0060/47#permutation2.py
+++ num_0041_0060/47#permutation2.py
@@ -2,20 +2,16 @@
 47. Permutations 2
 """
 
-def permute(nums):
-    return [[n] + p
-            for i, n in enumerate(nums)
-            for p in permute(nums[:i] + nums[i + 1:])] or [[]]
-
 def permuteUnique(nums):
-    res = []
-    for l in permute(nums):
-        if l not in res:
-            res.append(l)
-    return res
+    perms = [[]]
+    for n in nums:
+        perms = [p[:i] + [n] + p[i:]
+                    for p in perms
+                    for i in range((p + [n]).index(n) + 1)]
+    return perms
 
 def main():
-    nums = [1, 1, 2]
+    nums = [1, 1, 2, 2]
     print(permuteUnique(nums))
 
 if __name__ == "__main__":
