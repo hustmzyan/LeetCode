e58edc98397d92aaa3bd487bac4c0093b6147b5e
mzyan <mengzhao.yan@foxmail.com>
2019-05-15 15:31:37 +0800

    update 48#

--- num_0041_0060/48#rotateimage.py
+++ num_0041_0060/48#rotateimage.py
@@ -12,17 +12,20 @@ You have to rotate the image in-place, which means you have to modify the input 
 
 def rotate(A):
     n = len(A)
-    for i in range(int(n/2)):
-        for j in range(n-int(n/2)):
-            A[i][j], A[~j][i], A[~i][~j], A[j][~i] = \
-                     A[~j][i], A[~i][~j], A[j][~i], A[i][j]
+    for i in range(int(n / 2)):
+        for j in range(n - int(n / 2)):
+            # ~i 位运算，~i = n-i-1
+            # print(i, j, ~i, ~j)
+            A[i][j], A[~j][i], A[~i][~j], A[j][~i] = A[~j][i], A[~i][~j], A[j][~i], A[i][j]
+            # print(A)
 
 
 def main():
     mat = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]]
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]]
     print(mat)
     rotate(mat)
     print(mat)
--- test.py
+++
@@ -1,6 +0,0 @@
-
-for i in reversed(range(4)):
-    print(i)
-
-dp = [True] + [False] * 5
-print(dp)
