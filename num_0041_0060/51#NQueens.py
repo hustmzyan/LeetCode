0c667d84945a1ab251bd05b7ab36b6484f1969d1
mzyan <mengzhao.yan@foxmail.com>
2019-05-17 15:35:51 +0800

    add 51#N-Queens

---
+++ num_0041_0060/51#NQueens.py
@@ -0,0 +1,33 @@
+"""
+51. N-Queens
+
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+任何两个皇后不能处于同一行、同一列、同一斜线
+"""
+
+class Solution:
+    def solveNQueens(self, n: int):
+        def DFS(queens, xy_dif, xy_sum):
+            p = len(queens) # 当前解已确定的皇后个数
+            if p==n: # 满足条件，结束判定
+                result.append(queens)
+                return None
+            for q in range(n): # 逐行遍历
+                if q not in queens and p - q not in xy_dif and p + q not in xy_sum:
+                    print(queens, xy_dif, xy_sum)
+                    DFS(queens + [q], xy_dif + [p - q], xy_sum + [p + q])  # 深度优先进行递归
+                    
+        result = []
+        DFS([],[],[])
+        return [ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+
+def main():
+    print(Solution().solveNQueens(4))
+
+if __name__ == "__main__":
+    main()
