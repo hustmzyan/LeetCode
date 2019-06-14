38a6e02327e92e419d74da5f2176aaee9109e4c0
mzyan <mengzhao.yan@foxmail.com>
2019-06-14 08:44:36 +0800

    update 58#

--- num_0041_0060/58#lengthoflastword.py
+++ num_0041_0060/58#lengthoflastword.py
@@ -10,9 +10,17 @@ Note: A word is defined as a character sequence consists of non-space characters
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        ls = s.strip(' ').split(' ')
-        print(ls)
-        return len(ls[len(ls)-1])
+        ls = len(s)
+        # slow and fast pointers
+        slow = -1
+        # iterate over trailing spaces
+        while slow >= -ls and s[slow] == ' ':
+            slow-=1
+        fast = slow
+        # iterate over last word
+        while fast >= -ls and s[fast] != ' ':
+            fast-=1
+        return slow - fast
 
 def main():
     s = 'a b  abc   '
