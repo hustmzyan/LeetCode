d25cbebbc3977c1184a0396d6a83eca7427a446a
mzyan <mengzhao.yan@foxmail.com>
2019-06-16 10:59:50 +0800

    add 60#

---
+++ num_0041_0060/60#PermutationSequence.py
@@ -0,0 +1,39 @@
+"""
+60. Permutation Sequence
+
+The set [1,2,3,...,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+"""
+import math
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        numbers = list(range(1, n+1))
+        permutation = ''
+        k -= 1
+        while n > 0:
+            n -= 1
+            # get the index of current digit
+            index, k = divmod(k, math.factorial(n))
+            permutation += str(numbers[index])
+            # remove handled number
+            numbers.remove(numbers[index])
+
+        return permutation
+
+def main():
+    n = 3
+    k = 5
+    print(Solution().getPermutation(n, k))
+
+if __name__ == "__main__":
+    main()
