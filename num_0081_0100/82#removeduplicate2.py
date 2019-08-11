6d5ad9caf29cc2488888d436461aa24b88c9baaa
mzyan <mengzhao.yan@foxmail.com>
2019-08-11 08:39:31 +0800

    add 82#

---
+++ num_0081_0100/82#removeduplicate2.py
@@ -0,0 +1,60 @@
+"""
+82. Remove Duplicates from Sorted List II
+
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+Example 1:
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+Example 2:
+Input: 1->1->1->2->3
+Output: 2->3
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        dummy = pre = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                pre.next = head
+            else:
+                pre = pre.next
+                head = head.next
+        return dummy.next
+
+
+
+def main():
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(3)
+    node5 = ListNode(4)
+    node6 = ListNode(4)
+    node7 = ListNode(5)
+    
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next = node6
+    node6.next = node7
+
+    node = Solution().deleteDuplicates(node1)
+
+    while node.next:
+        print(node.val)
+        node = node.next
+
+if __name__ == "__main__":
+    main()
