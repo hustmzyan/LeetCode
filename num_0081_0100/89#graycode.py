873efed63536f9a1440c338092c4d66c128a98c7
mzyan <mengzhao.yan@foxmail.com>
2019-09-01 21:46:27 +0800

    update 89#

--- num_0081_0100/89#graycode.py
+++ num_0081_0100/89#graycode.py
@@ -1,7 +1,6 @@
 """
 89. Gray Code
 
-采用对称规律求解
 """
 
 class Solution:
@@ -10,7 +9,8 @@ class Solution:
             return [0]
         res = [0,1]
         for i in range(2,n+1):
-            for j in range(len(res)-1,-1,-1):
+            for j in range(len(res) - 1, -1, -1):
+                # res.append(res[j] | ( 1 << i ) - 1)
                 res.append(res[j] | 1 << i - 1)
         return res
 
