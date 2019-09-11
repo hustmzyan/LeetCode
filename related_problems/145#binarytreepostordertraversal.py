b796f35ebd373d05cfd3bda84e83550ee3ac70f1
mzyan <mengzhao.yan@foxmail.com>
2019-09-11 20:47:35 +0800

    add #145

--- related_problems/145#binarytreepostordertraversal.py
+++ related_problems/145#binarytreepostordertraversal.py
@@ -10,8 +10,37 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def postorderTraversal(self, root: TreeNode) -> List[int]:
-        pass
+    def postorderTraversal(self, root):
+        """
+        采用标志位的方法：
+            - 0 -> 左右子树未遍历
+            - 1 -> 左子树已遍历，右子树未被遍历
+            - 2 -> 左右子树已遍历
+        """
+        if not root:
+            return []
+        result, stack = [], [[root, 0]]
+        while stack:
+            cur, visited = stack[-1]
+            if visited == 0:
+                if cur.left:
+                    stack.append([cur.left, 0])
+                else:
+                    stack.pop()
+                    stack.append([cur, 1])
+            elif visited == 1:
+                if cur.right:
+                    stack.append([cur.right, 0])
+                else:
+                    stack.pop()
+                    stack.append([cur, 2])
+            else:
+                result.append(cur.val)
+                stack.pop()
+                if stack:
+                    stack[-1][-1] += 1
+
+        return result
 
 def main():
     node1 = TreeNode(1)
